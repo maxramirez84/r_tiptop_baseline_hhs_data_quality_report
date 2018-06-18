443f710bc7d11dddddc230b540b7cc9377b8dce6
maxramirez84 <maxramirez84@gmail.com>
2018-06-18 16:12:34 +0200

    After renaming non duplicated households and dropping duplicated households, could happen that still few duplicates remain in the dataset, concretely those household IDs which have occurrecies in both non duplicated and duplicated. They have been porcessed independently, now they must be treated on the whole by renaming again.

--- export_data_no_duplicates.R
+++ export_data_no_duplicates.R
@@ -5,18 +5,37 @@
 library(stringr)
 source("tiptop_hhs_quality.R")
 
+# Auxiliar functions
+renameRecords = function (data, record_ids_to_rename) {
+  previous_household_id = -1
+  for(i in 1:length(record_ids_to_rename)) {
+    new_household_id = paste0(
+      data$household[data$record_id == record_ids_to_rename[i]], '_01')
+    
+    if(new_household_id == previous_household_id)
+      new_household_id = paste0(
+        data$household[data$record_id == record_ids_to_rename[i]], '_02')
+    
+    data$household[data$record_id == record_ids_to_rename[i]] = new_household_id
+    
+    previous_household_id = new_household_id
+  }
+  
+  return(data)
+}
+
 # Read arguments
 args = commandArgs(T)
 #api_token = args[1]
-api_token = "1C249EF7F123F4DEB09EAAS7E4E9EDFS99" # TIPTOP HHS Baseline Madagascar (MALGACHE) TEMP
+api_token = "XXXXXX" # TIPTOP HHS Baseline DRC TEMP
 #study_area_1_id = args[2]
 #study_area_1    = args[3]
 #study_area_2_id = args[4]
 #study_area_2    = args[5]
 #study_areas_ids = c(study_area_1_id, study_area_2_id)
 #study_areas     = c(study_area_1, study_area_2) 
-study_areas_ids = c("mananjary", "toliary_2") # TEMP
-study_areas = c("Mananjary", "Toliary 2") # TEMP
+study_areas_ids = c("kenge", "bulungu") # TEMP
+study_areas = c("Kenge", "Bulungu") # TEMP
 
 # Read data set from REDCap by using the provided token
 redcap_api_url = "https://tiptop.isglobal.org/redcap/api/"
@@ -35,29 +54,22 @@ duplicated_households = duplicatedHouseholds(hhs_data, study_areas_ids, study_ar
 non_duplicated_households = duplicated_households[duplicated_households$duplicated == 'F', ]
 record_ids_to_rename = non_duplicated_households$record_id[
   duplicated(non_duplicated_households[2:4])]
-
-previous_household_id = -1
-for(i in 1:length(record_ids_to_rename)) {
-  new_household_id = paste0(
-    hhs_data_with_no_dups$household[hhs_data_with_no_dups$record_id == record_ids_to_rename[i]], 
-    '_01')
-  
-  if(new_household_id == previous_household_id)
-    new_household_id = paste0(
-      hhs_data_with_no_dups$household[hhs_data_with_no_dups$record_id == record_ids_to_rename[i]], 
-      '_02')
-  
-  hhs_data_with_no_dups$household[
-    hhs_data_with_no_dups$record_id == record_ids_to_rename[i]] = new_household_id
-  
-  previous_household_id = new_household_id
-}
+hhs_data_with_no_dups = renameRecords(hhs_data_with_no_dups, record_ids_to_rename)
 
 # When they are duplicates, remove them
-duplicated_households = duplicated_households[duplicated_households$duplicated == 'T', ]
-record_ids_to_drop = duplicated_households$record_id[duplicated(duplicated_households[2:4])]
+in_fact_duplicated_households = duplicated_households[duplicated_households$duplicated == 'T', ]
+record_ids_to_drop = in_fact_duplicated_households$record_id[
+  duplicated(in_fact_duplicated_households[2:4])]
 hhs_data_with_no_dups = hhs_data_with_no_dups[!(hhs_data_with_no_dups$record_id %in% 
                                                   record_ids_to_drop), ]
+
+# After renaming non duplicated households and dropping duplicated households, could happen that 
+# still few duplicates remain in the dataset, concretely those household IDs which have occurrecies 
+# in both non duplicated and duplicated. They have been porcessed independently, now they must be
+# treated on the whole by renaming again.
+duplicated_households = duplicatedHouseholds(hhs_data_with_no_dups, study_areas_ids, study_areas)
+record_ids_to_rename = duplicated_households$record_id[duplicated(duplicated_households[2:4])]
+hhs_data_with_no_dups = renameRecords(hhs_data_with_no_dups, record_ids_to_rename)
 
 # Report what have been done
 # Pending...
